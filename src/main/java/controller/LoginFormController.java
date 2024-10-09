392b577a3a3ed128b731f956d2a4d6f8f43b3900
chiranaimanjith <chiranaimangeth777@gmail.com>
2024-10-10 00:43:51 +0530

    first commit

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXTextField userName;
+
+    @FXML
+    private JFXPasswordField userPassword;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSigninOnAction(ActionEvent event) {
+
+    }
+
+}
